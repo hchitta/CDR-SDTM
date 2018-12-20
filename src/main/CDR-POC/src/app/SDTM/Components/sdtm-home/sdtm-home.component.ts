2377450cf12441e2e9a02d1f5b7a04948c4b47b7
mallikgithub <malakula@USATLMALAKULA9.us.deloitte.com>
2018-12-19 22:12:15 -0500

    UI changes

--- src/main/CDR-POC/src/app/SDTM/Components/sdtm-home/sdtm-home.component.ts
+++ src/main/CDR-POC/src/app/SDTM/Components/sdtm-home/sdtm-home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SeriesLabels } from '@progress/kendo-angular-charts';
 import { LegendLabels } from '@progress/kendo-angular-charts';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { CollapseDirective } from 'ngx-bootstrap';
 
 @Component({
   selector: 'sdtm-home',
@@ -13,6 +14,7 @@ export class SdtmHomeComponent implements OnInit {
   navBarItems: Object[];
   appName: string;
   public isCollapsed = false;
+
   public pieData: any = [
     { category: 'In Progress', value: 20 },
     { category: 'Ready for Review', value: 11 },
@@ -25,40 +27,40 @@ export class SdtmHomeComponent implements OnInit {
   public seriesData: any[] = [{
     study: 'This Study',
     domains: 35
-    }];
+  }];
 
-    public seriesDataTwo: any[] = [{
-      study: 'Overdue',
-      domains: 3,
-      color: 'red'
-      },
-      {
-        study: 'Due in 7 Days',
-        domains: 7,
-        color: 'brown'
-      }];
+  public seriesDataTwo: any[] = [{
+    study: 'Overdue',
+    domains: 3,
+    color: 'red'
+  },
+  {
+    study: 'Due in 7 Days',
+    domains: 7,
+    color: 'brown'
+  }];
 
-      public seriesLabels: SeriesLabels = {
-      visible: true, // Note that visible defaults to false
-        padding: 3,
-        font: 'bold 12px Open Sans',
-      };
+  public seriesLabels: SeriesLabels = {
+    visible: true, // Note that visible defaults to false
+    padding: 3,
+    font: 'bold 12px Open Sans',
+  };
 
-      public seriesPieLabels: SeriesLabels = {
-        visible: true, // Note that visible defaults to false
-          font: 'bold 10px Open Sans',
-          color: 'white',
-          position: 'center',
-        };
+  public seriesPieLabels: SeriesLabels = {
+    visible: true, // Note that visible defaults to false
+    font: 'bold 10px Open Sans',
+    color: 'white',
+    position: 'center',
+  };
 
-        public legendLabels: LegendLabels = {
-            font: '10px Open Sans',
-            color: 'black'
-          };
+  public legendLabels: LegendLabels = {
+    font: '10px Open Sans',
+    color: 'black'
+  };
 
-      public hidden: any = { visible: false };
+  public hidden: any = { visible: false };
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.appName = " - Path to SDTM";
@@ -68,9 +70,15 @@ export class SdtmHomeComponent implements OnInit {
       { "navBarTitle": "Business Rule Configuration", "navBarLink": "businessRules" },
       { "navBarTitle": "Job Execution", "navBarLink": "jobExecution" }];
 
-      this.http.get<any[]>(`/api/CDR/pathToSdtmDashBoard`).subscribe(data => {
-        this.dashBoardData = data;
-      });
+    this.http.get<any[]>(`/api/CDR/pathToSdtmDashBoard`).subscribe(data => {
+      this.dashBoardData = data;
+    });
+  }
+
+
+  collapse() {
+    alert("clicked")
+    this.isCollapsed = !this.isCollapsed;
   }
 
 }
